f0743036dfc2a93f3a52c602ed1702f70706cffc
Michael <michael@sidis.(none)>
2010-06-07 22:23:59 -0400

    Draw lines between centers

--- Deem/src/com/coinjema/Deem/Grid.java
+++ Deem/src/com/coinjema/Deem/Grid.java
@@ -1,82 +1,117 @@
 package com.coinjema.Deem;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.ArrayList;
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Grid {
+	private static ExecutorService exec = Executors.newFixedThreadPool(Runtime
+			.getRuntime().availableProcessors());
 
-	protected final List<Area> locations;
-	protected final int numCols, numRows;
-	protected final double distanceBetweenCenters;
+	protected final Point2D[] locations;
+	private List<Point> drawn;
+	private final Dimension bounds;
 
-	public Grid(int numCols, int numRows, double distanceFromEach) {
-		this.numCols = numCols;
-		this.numRows = numRows;
-		this.distanceBetweenCenters = distanceFromEach;
-		locations = new ArrayList<Area>(numCols * numRows);
-		initLocations(locations);
+	public Grid(Dimension bounds, Point2D... centers) {
+		locations = centers;
+		this.bounds = bounds;
 		System.out.println("Done initializing grid");
 	}
 
-	protected void initLocations(List<Area> locations) {
-		for (int i = 0; i < getNumCols(); i++) {
-			double x = (i * distanceBetweenCenters)
-					+ (distanceBetweenCenters / 2);
-			for (int j = 0; j < getNumRows(); j++) {
-				double y = (j * distanceBetweenCenters)
-						+ (distanceBetweenCenters / 2);
-				locations.add(new Square(x, y, (distanceBetweenCenters / 2)));
-			}
-		}
-	}
-
-	protected int getNumCols() {
-		return numCols;
-	}
-
-	protected int getNumRows() {
-		return numRows;
-	}
-
 	public void draw(Graphics g) {
-		for (Area a : locations) {
-			for (Coord adj : getAdjacentCenters(a)) {
-				Coord middle = new Coord((a.col + adj.col) / 2,
-						(a.row + adj.row) / 2);
-				g
-						.drawLine(
-								(int) middle.col,
-								(int) middle.row,
-								(int) (middle.col + ((adj.row - a.row) / distanceBetweenCenters)
-										* getHalfBorderLength()),
-								(int) (middle.row - ((adj.col - a.col) / distanceBetweenCenters)
-										* getHalfBorderLength()));
-				g
-						.drawLine(
-								(int) middle.col,
-								(int) middle.row,
-								(int) (middle.col - ((adj.row - a.row) / distanceBetweenCenters)
-										* getHalfBorderLength()),
-								(int) (middle.row + ((adj.col - a.col) / distanceBetweenCenters)
-										* getHalfBorderLength()));
-			}
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, (int) bounds.getWidth(), (int) bounds.getHeight());
+		g.setColor(Color.black);
+		if (drawn == null) {
+			calcPoints();
+		}
+		for (Point p : drawn) {
+			g.fillRect(p.x, p.y, 1, 1);
 		}
 	}
 
-	protected double getHalfBorderLength() {
-		return distanceBetweenCenters / 2;
+	private void calcPoints() {
+		drawn = Collections.synchronizedList(new LinkedList<Point>());
+		long time = System.currentTimeMillis();
+		for (final Point2D point : iterate(bounds)) {
+			exec.execute(
+			new Runnable() {
+				public void run() {
+					double lowest = Double.MAX_VALUE;
+					boolean multiple = false;
+					for (Point2D center : locations) {
+						double dist = point.distance(center);
+						if (Math.abs(dist - lowest) < 1d) {
+							lowest = Math.min(dist, lowest);
+							multiple = true;
+						} else if (dist < lowest) {
+							lowest = dist;
+							multiple = false;
+						}
+					}
+					if (multiple) {
+						drawn.add(new Point((int) point.getX(), (int) point
+								.getY()));
+					}
+				}
+			});
+		}
+		try {
+			exec.shutdown();
+			exec.awaitTermination(1,TimeUnit.HOURS);
+			System.out.println("Grid calc took " + (System.currentTimeMillis() - time) + " ms");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	protected Iterable<Coord> getAdjacentCenters(Area a) {
-		List<Coord> adj = new LinkedList<Coord>();
-		adj.add(new Coord(a.col + distanceBetweenCenters, a.row));
-		adj.add(new Coord(a.col - distanceBetweenCenters, a.row));
-		adj.add(new Coord(a.col, a.row + distanceBetweenCenters));
-		adj.add(new Coord(a.col, a.row - distanceBetweenCenters));
-		return adj;
+	private Iterable<Point2D> iterate(final Dimension bounds) {
+		// TODO Auto-generated method stub
+		return new Iterable<Point2D>() {
+			@Override
+			public Iterator<Point2D> iterator() {
+				// TODO Auto-generated method stub
+				return new Iterator<Point2D>() {
+					int x = -1;
+					int y = 0;
+
+					@Override
+					public void remove() {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public Point2D next() {
+						x += 1;
+						if (x < bounds.getWidth()) {
+							return new Point2D.Double(x, y);
+						} else {
+							x = 0;
+							y += 1;
+							return new Point2D.Double(x, y);
+						}
+					}
+
+					@Override
+					public boolean hasNext() {
+						return x + 1 < bounds.getWidth()
+								|| y + 1 < bounds.getHeight();
+					}
+				};
+			}
+		};
 	}
 
 }
--- Deem/src/com/coinjema/Deem/GridFrame.java
+++ Deem/src/com/coinjema/Deem/GridFrame.java
@@ -6,15 +6,17 @@ import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Point2D;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
 public class GridFrame extends JFrame {
-	
+
 	public Grid grid;
-	
-	
-	
+
 	public GridFrame() throws HeadlessException {
 		super();
 		// TODO Auto-generated constructor stub
@@ -39,7 +41,7 @@ public class GridFrame extends JFrame {
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paint(g);
-		g.translate(50,50);
+		g.translate(50, 50);
 		try {
 			grid.draw(g);
 		} catch (Exception e) {
@@ -62,10 +64,56 @@ public class GridFrame extends JFrame {
 				System.exit(0);
 			}
 		});
-		mainWindow.setPreferredSize(new Dimension(1200,1000));
-		mainWindow.grid = new HexGrid(30,30,40);
+		mainWindow.setPreferredSize(new Dimension(1200, 1000));
+		mainWindow.grid = new Grid(mainWindow.getPreferredSize(), getCenters(
+				1200, 1000, 400));
 		mainWindow.pack();
 		mainWindow.setVisible(true);
+	}
+
+	private static Point2D[] getCenters(int width, int height, int numPoints) {
+		Point2D[] centers = new Point2D[numPoints];
+		double xdiff = width / Math.sqrt(numPoints);
+		double ydiff = height / Math.sqrt(numPoints);
+		double x = 0;
+		double y = ydiff;
+		for (int i = 0; i < numPoints; i++) {
+			x += xdiff;
+			if (x >= width) {
+				x = xdiff;
+				y += ydiff;
+				centers[i] = new Point2D.Double(x, y);
+			} else {
+				centers[i] = new Point2D.Double(x, y);
+			}
+		}
+		return centers;
+	}
+
+	private static Point2D[] getRandomCenters(int numFailures, double minDist,
+			int maxX, int maxY) {
+		List<Point2D> centers = new LinkedList<Point2D>();
+		Random rand = new Random();
+		int failures = 0;
+		do {
+			double x = rand.nextDouble() * maxX;
+			double y = rand.nextDouble() * maxY;
+			boolean fail = false;
+			Point2D newPoint = new Point2D.Double(x, y);
+			for (Point2D p : centers) {
+				if (p.distance(newPoint) < minDist) {
+					fail = true;
+					break;
+				}
+			}
+			if (fail) {
+				failures++;
+			} else {
+				failures = 0;
+				centers.add(newPoint);
+			}
+		} while (failures < numFailures);
+		return centers.toArray(new Point2D[centers.size()]);
 	}
 
 }
